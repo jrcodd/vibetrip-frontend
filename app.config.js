d007692b2a3f887a3037f0890a86ab697d43a525
Jackson Codd <coddjackson@gmail.com>
2025-06-08 18:14:13 -0400

    e

--- app.config.js
+++ app.config.js
@@ -11,15 +11,23 @@ export default {
             resizeMode: "contain",
             backgroundColor: "#ffffff"
         },
+        ios: {
+            bundleIdentifier: "com.jcodd.vibetrip",
+            infoPlist: {
+                "ITSAppUsesNonExemptEncryption": false
+            }
+        },
         extra: {
             supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
             supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
-        },
+            eas: {
+                projectId: "439402d5-3cab-402d-afb2-3aef364d108e"
+            },
 
-        scheme: "vibetrip",
-        plugins: [
-            "expo-router"
-        ]
+            scheme: "vibetrip",
+            plugins: [
+                "expo-router"
+            ]
+        }
     }
-
 }
